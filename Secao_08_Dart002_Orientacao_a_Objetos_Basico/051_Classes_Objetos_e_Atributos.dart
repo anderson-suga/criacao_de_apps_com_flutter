367de9095d9bf1c1cf037102df5b7b8701045590
Anderson Akihiro Suga <anderson.suga@gmail.com>
2022-03-30 14:25:11 -0300

    Update 051_Classes_Objetos_e_Atributos.dart

---
+++ Secao_08_Dart002_Orientacao_a_Objetos_Basico/051_Classes_Objetos_e_Atributos.dart
@@ -0,0 +1,23 @@
+void main() {
+  Pessoa pessoa1 = new Pessoa();
+  pessoa1.nome = 'Anderson';
+  pessoa1.idade = 15;
+  pessoa1.casado = true;
+  print(pessoa1.nome);
+  print(pessoa1.idade);
+  print(pessoa1.casado);
+
+  print('\n----------------------------------------------------\n');
+
+  Pessoa pessoa2 = new Pessoa();
+  pessoa2.nome = 'Alguém';
+  print(pessoa2.nome);
+  print(pessoa2.idade);
+  print(pessoa2.casado);
+}
+
+class Pessoa {
+  String? nome;
+  int? idade;
+  bool casado = false;
+}
