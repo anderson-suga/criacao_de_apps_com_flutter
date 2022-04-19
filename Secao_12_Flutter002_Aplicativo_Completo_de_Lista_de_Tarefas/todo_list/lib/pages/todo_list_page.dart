aa1578cef15edb3851cea77b55187b8aa3d589ad
Anderson Suga <anderson.suga@gmail.com>
2022-04-19 16:40:15 -0300

    Aula 88. Personalizando o Campo de Texto

--- Secao_12_Flutter002_Aplicativo_Completo_de_Lista_de_Tarefas/todo_list/lib/pages/todo_list_page.dart
+++ Secao_12_Flutter002_Aplicativo_Completo_de_Lista_de_Tarefas/todo_list/lib/pages/todo_list_page.dart
@@ -5,8 +5,34 @@ class TodoListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return const Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 16,
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+                labelText: 'Preço',
+                hintText: 'user@exemplo.com',
+                //border: InputBorder.none,
+                border: OutlineInputBorder(),
+                errorText: null,
+                prefixText: 'R\$ ',
+                suffixText: 'cm',
+                labelStyle: TextStyle(
+                  fontSize: 40,
+                  color: Colors.deepPurple,
+                )),
+            // obscureText: true,
+            keyboardType: TextInputType.number,
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              color: Colors.deepOrange,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
