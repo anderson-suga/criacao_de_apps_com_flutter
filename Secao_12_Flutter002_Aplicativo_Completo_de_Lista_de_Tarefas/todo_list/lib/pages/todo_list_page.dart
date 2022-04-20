a533d5d48953dc3c4196953fe08a3dedb12180a3
Anderson Suga <anderson.suga@gmail.com>
2022-04-20 17:56:44 -0300

    Aula 93. Botão de Limpar Tudo

--- Secao_12_Flutter002_Aplicativo_Completo_de_Lista_de_Tarefas/todo_list/lib/pages/todo_list_page.dart
+++ Secao_12_Flutter002_Aplicativo_Completo_de_Lista_de_Tarefas/todo_list/lib/pages/todo_list_page.dart
@@ -9,30 +9,52 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              const Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Adicione uma tarefa',
-                    hintText: 'Ex.: Estudar flutter',
+              Row(
+                children: [
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma tarefa',
+                        hintText: 'Ex.: Estudar flutter',
+                      ),
+                    ),
                   ),
-                ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xff00d7f3),
+                      padding: const EdgeInsets.all(14),
+                    ),
+                    child: const Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(
-                width: 8,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style:  ElevatedButton.styleFrom(
-                  primary: const Color(0xff00d7f3),
-                  padding: const EdgeInsets.all(14),
-                ),
-                child: const Icon(
-                  Icons.add,
-                  size: 30,
-                ),
+              const SizedBox(height: 32),
+              Row(
+                children: [
+                  const Expanded(
+                    child: Text('Você possui 0 tarefa(s) pendente(s)'),
+                  ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xff00d7f3),
+                      padding: const EdgeInsets.all(14),
+                    ),
+                    child: Text('Limpar tudo'),
+                  ),
+                ],
               ),
             ],
           ),
