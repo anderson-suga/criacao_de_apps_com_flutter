059de14bb3c745f0f1fe979fb459439aaa7db03e
Anderson Suga <anderson.suga@gmail.com>
2022-04-20 17:38:42 -0300

    Aula 91. Trabalhando com o Expanded

--- Secao_12_Flutter002_Aplicativo_Completo_de_Lista_de_Tarefas/todo_list/lib/pages/todo_list_page.dart
+++ Secao_12_Flutter002_Aplicativo_Completo_de_Lista_de_Tarefas/todo_list/lib/pages/todo_list_page.dart
@@ -1,51 +1,36 @@
 import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
-  TodoListPage({Key? key}) : super(key: key);
-
-  final TextEditingController emailController = TextEditingController();
+  const TodoListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 16,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
             children: [
-              TextField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  labelText: 'E-mail',
+              const Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Adicione uma tarefa',
+                    hintText: 'Ex.: Estudar flutter',
+                  ),
                 ),
-                onChanged: onChanged,
-                onSubmitted: onSubmitted,
+              ),
+              const SizedBox(
+                width: 8,
               ),
               ElevatedButton(
-                onPressed: login,
-                child: Text('Entrar'),
+                onPressed: () {},
+                child: Text('+'),
               ),
             ],
           ),
         ),
       ),
     );
-  }
-
-  void login() {
-    String text = emailController.text;
-    debugPrint('login: $text');
-    emailController.clear();
-  }
-
-  void onChanged(String text) {
-    debugPrint('onChanged: $text');
-  }
-
-  void onSubmitted(String text) {
-    debugPrint('onSubmitted: $text');
   }
 }
