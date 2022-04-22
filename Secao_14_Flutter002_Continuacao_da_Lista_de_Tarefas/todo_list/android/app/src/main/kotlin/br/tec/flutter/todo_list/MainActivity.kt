b6e5c2009f54518974b2471b5e88f1204f58d841
Anderson Suga <anderson.suga@gmail.com>
2022-04-22 16:00:27 -0300

    Aula 109. Introdução ao shared_preferences

---
+++ Secao_14_Flutter002_Continuacao_da_Lista_de_Tarefas/todo_list/android/app/src/main/kotlin/br/tec/flutter/todo_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.tec.flutter.todo_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Secao_14_Flutter002_Continuacao_da_Lista_de_Tarefas/todo_list/lib/repositories/todo_repository.dart
@@ -0,0 +1,14 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TodoRepository {
+  TodoRepository() {
+    SharedPreferences.getInstance().then((value) => sharedPreferences = value);
+  }
+
+  late SharedPreferences sharedPreferences;
+
+  void exemplo() {
+    sharedPreferences.setString('nome', 'Anderson');
+    sharedPreferences.getString('nome');
+  }
+}
