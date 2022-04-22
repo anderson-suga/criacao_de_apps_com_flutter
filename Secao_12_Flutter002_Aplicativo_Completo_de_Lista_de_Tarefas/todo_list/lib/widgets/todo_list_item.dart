46a58f1c90bc007216e850c95a92d819d8a4beb6
Anderson Suga <anderson.suga@gmail.com>
2022-04-22 11:20:07 -0300

    Aula 98. Melhorando a Interface do App

--- Secao_12_Flutter002_Aplicativo_Completo_de_Lista_de_Tarefas/todo_list/lib/widgets/todo_list_item.dart
+++ Secao_12_Flutter002_Aplicativo_Completo_de_Lista_de_Tarefas/todo_list/lib/widgets/todo_list_item.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class TodoListItem extends StatelessWidget {
-  const TodoListItem({Key? key}) : super(key: key);
+  const TodoListItem({Key? key, required this.title}) : super(key: key);
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
@@ -11,18 +13,19 @@ class TodoListItem extends StatelessWidget {
         color: Colors.grey[300],
       ),
       padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.symmetric(vertical: 2),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          Text(
+        children: [
+          const Text(
             '20/11/2021',
             style: TextStyle(
               fontSize: 12,
             ),
           ),
           Text(
-            'Task X',
-            style: TextStyle(
+            title,
+            style: const TextStyle(
               fontSize: 16,
               fontWeight: FontWeight.w600,
             ),
